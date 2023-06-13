b3f60a893a1719aaa24c7a6ebad70397c8947fa8
Nghi D. Q. Bui <bdqnghi@gmail.com>
2023-06-13 20:01:11 +0700

    update

--- pages/Technique_Explanation.py
+++ pages/Technique_Explanation.py
@@ -1,4 +1,64 @@
 import streamlit as st
+from PIL import Image
 
 st.markdown("# Technique Explanation ❄️")
 st.sidebar.markdown("# Technique Explanation ❄️")
+
+st.write("This demo leverages machine learning models to automatically \
+    generate the missing TXL rule, denoted as c. The goal is to ensure \
+    that the combined rule set C', formed by integrating the newly predicted TXL \
+    rule c with the existing context rule set C, can successfully transform \
+        the given **code before** into the desired **code after**. ")
+
+st.markdown("## 1. Dataset Collection")
+st.write("We initially gathered triplets comprising of **(code before, code after, TXL rules)**, \
+    where the TXL rules are capable of transforming the **code before** into the **code after**. \
+    The data was sourced from three different parts as outlined below:")
+
+
+lst = ['Standard examples from the official website [1];', 
+       'C2rust examples from the paper transforming C to Rust [2];',  
+       'Rosseta code (317) instances used in inner source C2Rust experiments.']
+
+s = ''
+
+for i in lst:
+    s += "- " + i + "\n"
+
+st.markdown(s)
+
+st.markdown("## 2. Dataset Preprocessing")
+st.write("Due to the scarcity of TXL rule transformations, \
+    the limited number of available TXL transformations poses challenges \
+        in effectively applying machine learning techniques. Additionally, \
+        identifying the most relevant rule for achieving the desired conversion \
+        becomes more difficult. In order to overcome these issues, we have developed \
+        a data extraction method that enables the generation of all possible TXL \
+        transformations.")
+
+outer_cols = st.columns([1, 1])
+with outer_cols[0]:
+    image = Image.open('/Users/lichunmiao/SynologyDrive/rulegen_frontend/images/txl_big.jpg')
+    st.image(image, caption='Original TXL rules.')
+with outer_cols[1]:
+    image = Image.open('/Users/lichunmiao/SynologyDrive/rulegen_frontend/images/txl_alg1_1.jpg')
+    st.image(image, caption='TXL rules produced by the data extraction method.')
+    
+st.markdown("We extracted all possible TXL transformations from the original TXL rules.\
+ First, we analyzed the rule call graph of the original TXL rules (refer to the left image above).\
+ Next, we sliced each path from the main rule to a leaf rule in the call graph to generate a new set of TXL rules (refer to the right image above).\
+ Afterwards, we obtained multiple **code after** by applying the newly created TXL rules to **code before**.\
+ Finally, for a set of TXL rules, we designated each rule as a hole and retained the remaining rules as **context**, allowing us to construct the training dataset.\
+ The dataset comprises quadruples, each consisting of the following elements: **(code before, code after, context rules, hole rules)**. \
+    ")
+
+
+st.markdown("## 3. Model Training")
+st.write("We use the preprocessed dataset to train a machine learning model ")
+
+st.markdown("## 4. Performance Evaluation")
+
+
+st.markdown("## References")
+st.write("[1] TXL Resources. (n.d.). Retrieved June 7, 2023, from https://www.txl.ca/txl-resources.html")
+st.write("[2] In rust we trust – a transpiler from unsafe C to safer rust, M Ling. et al.")
--- prototype.py
+++ prototype.py
@@ -22,17 +22,79 @@ st.markdown('This is a workbench for predicting missing TXL rules. Upload **code
 
 # UI Layout
 col1, col2, col3 = st.columns(3)
-samples = ['Sample 1', 'Sample 2', 'Sample 3']  # Replace these with actual sample names
-selected_sample = st.selectbox('Or choose a predefined sample', samples)
+# Define your samples
+sample_keys = ['Sample 1', 'Sample 2', 'Sample 3', 'Sample 4', 'Sample 5']
+samples = {}
+
+# Loop through the sample keys to populate the dictionary
+for key in sample_keys:
+    before_path = f'samples/{key.lower().replace(" ", "_")}/before.txt'
+    after_path = f'samples/{key.lower().replace(" ", "_")}/after.txt'
+    context_path = f'samples/{key.lower().replace(" ", "_")}/context.txt'
+
+    # Check that the files exist before trying to read them
+    if os.path.exists(before_path) and os.path.exists(after_path) and os.path.exists(context_path):
+        with open(before_path, 'r') as bf, open(after_path, 'r') as af, open(context_path, 'r') as cf:
+            before = bf.read()
+            after = af.read()
+            context = cf.read()
+
+        samples[key] = {
+            'before': before,
+            'after': after,
+            'context': context,
+        }
+
+selected_sample_key = st.selectbox('Or choose a predefined sample', sample_keys)
+
+# Fetch the selected sample
+selected_sample = samples.get(selected_sample_key)
+
+
+existing_contexts = ['Array', 'Atoi', 'Const2mut', 'Errno Location', 'Fn', 'Formalize Code', 'Main', 'Null', 'Stdio', 'Time', 'Unsafe', 'Var Type No Bounds']
+contexts = {}
+
+# Loop through the sample keys to populate the dictionary
+for context in existing_contexts:
+    context_path = f'context/{context.lower().replace(" ", "_")}.txl'  # Replace 'key' with 'context' and fix file extension
+  
+    # Check that the files exist before trying to read them
+    if os.path.exists(context_path):
+        with open(context_path, 'r') as context_f:  # Add colon (:)
+            context_data = context_f.read()
+            
+        # Populate dictionary
+        contexts[context] = context_data
+
+# Add dropdown for contexts in Streamlit
+selected_context = st.selectbox('Choose an existing context', list(contexts.keys()), key="context_file")
+
+# Fetch the selected context
+selected_context_data = contexts.get(selected_context)
+
+if selected_context_data:
+    context_input_text_default = selected_context_data
+
+
+# If a sample was selected, populate the text inputs with its data
+if selected_sample:
+    before_input_text_default = selected_sample['before']
+    after_input_text_default = selected_sample['after']
+    context_input_text_default = selected_sample['context']
+else:
+    before_input_text_default = ""
+    after_input_text_default = ""
+    context_input_text_default = ""
+
 with col1:
     st.markdown("## Code before")
     uploaded_before_file = st.file_uploader(
-        label="Upload a .txt file here",
+        label="Upload a **code before** file with .txt extension",
         type=["txt"],
         accept_multiple_files=False,
         help="Scanned documents are not supported yet!"
     )
-    before_input_text = st.text_area("OR input the code here", key="before")
+    before_input_text = st.text_area("OR input the code here", value=before_input_text_default, key="before", height=300)
 
 with col2:
     st.markdown("## Code after")
@@ -42,7 +104,7 @@ with col2:
         accept_multiple_files=False,
         help="Scanned documents are not supported yet!"
     )
-    after_input_text = st.text_area("OR input the code here", key="after")
+    after_input_text = st.text_area("OR input the code here", value=after_input_text_default, key="after", height=300)
 
 with col3:
     st.markdown("## Context rules")
@@ -52,25 +114,70 @@ with col3:
         accept_multiple_files=False,
         help="Scanned documents are not supported yet!"
     )
-    context_input_text = st.text_area("OR input the code here", key="context")
+    context_input_text = st.text_area("OR input the code here", value=context_input_text_default, key="context", height=300)
+
 
 # Model selection
 models = ['CodeT5', 'CodeGen', 'StarCoder']
 selected_model = st.selectbox('Choose the model to predict the missing rule', models, key="model")
 
 # Additional parameters
-temperature = st.slider('Temperature', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+temperature = st.slider('Temperature', min_value=0.0, max_value=1.0, value=0.95, step=0.01)
 max_length = st.slider('Max Length', min_value=0, max_value=1024, value=512, step=1)
+
+# Initialize session state
+if 'predict_clicked' not in st.session_state:
+    st.session_state['predict_clicked'] = False
 
 # Predict button
 if st.button('Predict'):
+    st.session_state['predict_clicked'] = True
     # Data Inference
     data_inf = {
-       'before': before_input_text,
-       'after': after_input_text,
-       'context': context_input_text
+        'before': before_input_text,
+        'after': after_input_text,
+        'context': context_input_text
     }
-    data_inf = pd.DataFrame([data_inf])
-    st.write('## The predicted missing rule is as below: ')
-    missing_rule="nihao"
-    st.download_button('Download the predicted missing rule', missing_rule)
+    # POST request to Flask API
+    url = 'http://localhost:5000/predict'  # Replace with your Flask API URL
+    with st.spinner('Predicting...'):
+        response = requests.post(url, json=data_inf)
+
+    if response.status_code == 200:
+        missing_rule = response.json()['missing_rule']
+        st.write('## The predicted missing rule is as below: ')
+        st.code(missing_rule, language='python')  # Display missing_rule as a code block
+
+        # Create two columns for the buttons
+        
+        st.download_button('Download the predicted missing rule', missing_rule)
+            
+     
+        st.session_state['missing_rule'] = missing_rule
+    else:
+        st.error('Failed to predict. Please check your inputs and try again.')
+
+if st.session_state['predict_clicked']:
+    # Execute Rule button
+    if st.button('Execute Rule'):
+        # POST request to Flask API to execute the rule
+        url_execute = 'http://localhost:5000/execute'  # Replace with your Flask API URL for executing the rule
+
+        combined_rule = context_input_text + '\n' + st.session_state['missing_rule']
+
+        data_execute = {
+            'program': before_input_text,  # assuming 'before' code is the program to be transformed
+            'txl': combined_rule  # use the predicted missing rule as TXL script
+        }
+        with st.spinner('Executing...'):
+            response_execute = requests.post(url_execute, json=data_execute)
+      
+        if response_execute.status_code == 200:
+            execution_result = response_execute.json()['transformed_program']
+            st.session_state['execution_result'] = execution_result
+            st.text_area('Execution Result:', value=st.session_state['execution_result'], height=200, key='execution_result')
+        else:
+            st.error('Failed to execute the rule.')
+
+if 'execution_result' in st.session_state:
+    st.text_area('Execution Result:', value=st.session_state['execution_result'], height=200, key='execution_result')
